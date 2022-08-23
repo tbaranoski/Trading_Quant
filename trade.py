0cc3bbd01dd8e08f352a50293a173b57def1e65b
tbaranoski <taylorbaranoski1@gmail.com>
2022-08-22 22:58:05 -0400

    end of day save...

--- trade.py
+++ trade.py
@@ -10,6 +10,25 @@ import alpaca_trade_api as tradeapi #addded line
 import market_health
 from config import *
 
+#imports for alpha_vantage package
+from alpha_vantage.alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.alpha_vantage.techindicators import TechIndicators
+import matplotlib.pyplot as plt
+
+#test for alpha_vantage
+#def get_ema(self, symbol, interval='daily', time_period=20, series_type='close'):
+ti = TechIndicators(key= API_KEY, output_format='pandas')
+data, meta_data = ti.get_ema(symbol='MSFT', interval='daily', time_period=21)
+#data.plot()
+#plt.title('21 EMA indicator for  MSFT stock (DAILY)')
+#plt.show()
+
+#ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
+#data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+#data['4. close'].plot()
+#plt.title('Intraday Times Series for the MSFT stock (1 min)')
+#plt.show()
+##############################################################################
 BASE_URL = "https://paper-api.alpaca.markets"
 ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
 ORDERS_URL = "{}/v2/orders".format(BASE_URL)
@@ -56,5 +75,5 @@ def main():
     distribution_array_indexes = market_health.get_distribution_health(api)
     market_health.get_ema_health(api)
 
-    
+
 main()
