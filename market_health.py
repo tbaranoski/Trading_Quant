eed731d90632deed1726856f46be381d287502ed
tbaranoski <taylorbaranoski1@gmail.com>
2022-08-22 20:14:33 -0400

    Issue #9 Print Function for Distribution Day

--- market_health.py
+++ market_health.py
@@ -25,17 +25,29 @@ SHORT_DISTRIBUTION_NUMBER = 7
 #CONSTANT COMPUTATIONS
 #To account for Sunday and Saturday which are not actual tradin days. + 10 accounts for any holidays
 LONG_DISTRIBUTION = math.ceil((LONG_DISTRIBUTION_NUMBER *((9/7)) + 10))
-print("The Long distribution is: ", LONG_DISTRIBUTION)
 SHORT_DISTRIBUTION =  math.ceil((SHORT_DISTRIBUTION_NUMBER *((9/7)) + 10))
 
 #############################################################################################################################################
+#Print Distribution Days for Major Indexes
+def print_distribution_day(distribution_array):
+    print("Distribution Day Count($SPY) over last",LONG_DISTRIBUTION_NUMBER, " days: ", distribution_array[0])
+    print("Distribution Day Count($SPY) over last",SHORT_DISTRIBUTION_NUMBER, " days: ", distribution_array[1])
+    print("Distribution Day Count($QQQ) over last",LONG_DISTRIBUTION_NUMBER, " days: ", distribution_array[2])
+    print("Distribution Day Count($QQQ) over last",SHORT_DISTRIBUTION_NUMBER, " days: ", distribution_array[3])
+    print("Distribution Day Count($DIA) over last",LONG_DISTRIBUTION_NUMBER, " days: ", distribution_array[4])
+    print("Distribution Day Count($DIA) over last",SHORT_DISTRIBUTION_NUMBER, " days: ", distribution_array[5])
+    print("Distribution Day Count($IWO) over last",LONG_DISTRIBUTION_NUMBER, " days: ", distribution_array[6])
+    print("Distribution Day Count($IWO) over last",SHORT_DISTRIBUTION_NUMBER, " days: ", distribution_array[7])
+    print("Distribution Day Count($IWM) over last",LONG_DISTRIBUTION_NUMBER, " days: ", distribution_array[8])
+    print("Distribution Day Count($IWM) over last",SHORT_DISTRIBUTION_NUMBER, " days: ", distribution_array[9])
 
-#FOR TESTING
+#FOR TESTING BAR VALUES
 def print_Close(NUM_DAYS, TICKER_D_BARS):
     lenght_data = len(TICKER_D_BARS)
     for i in range (lenght_data):
         print("Index: ", i, "Date: ", TICKER_D_BARS[i].t, "Close: ", TICKER_D_BARS[i].c )
-
+############################################################################################################################################
+############################################################################################################################################
 
 ##Returns Distribution Day Count
 def get_Distribution_DAY_COUNT(NUM_DAYS, TICKER_D_BARS):
@@ -44,20 +56,17 @@ def get_Distribution_DAY_COUNT(NUM_DAYS, TICKER_D_BARS):
     #Get Start Index
     start_index = (len(TICKER_D_BARS)) - NUM_DAYS
     end_index = (len(TICKER_D_BARS)) - 1
-    print (start_index)
-    print (end_index)
 
-
+    #Itterate through start and end index
     for i in range (start_index, end_index):
         #Calculate percentage chnage from yesterdays close
         change_percent = ((((TICKER_D_BARS[i+1].c) - (TICKER_D_BARS[i].c)) / (TICKER_D_BARS[i].c))) * 100
-        #print("Change %: ", change_percent, "Close is: ", TICKER_D_BARS[i].c, "Date is: ", TICKER_D_BARS[i].t)
 
         # IF index dropped by more than .2% look at volume comparison between yesterday and today
         if(change_percent <= PERCENT_TO_BE_DISTRIBUTION):
             if((TICKER_D_BARS[i+1].v - (TICKER_D_BARS[i].v)) > 0):                
                 distribution_days = distribution_days + 1
-                #print("Distribution Day\n")
+                
 
     return distribution_days
 #############################################################################################################################################
@@ -74,10 +83,7 @@ def get_Market_health(api):
     #Calculate start_time since get_bars works in reverse for get_bars (V2 endpoint)
     #Reference https://forum.alpaca.markets/t/get-bars-vs-get-barset/8127/6
     timeNow = dt.datetime.now(pytz.timezone('US/Eastern'))
-    print("\n\nThe time now is: ", timeNow)
-
     start_time_long_distribution = timeNow - dt.timedelta(days=LONG_DISTRIBUTION)
-    start_time_short_distribution = timeNow - dt.timedelta(days=SHORT_DISTRIBUTION)
 
     #Get DAILY BARS for SPY and QQQ and DIA, IWO, and IWM
     SPY_D_BARSET = api.get_bars('SPY', TimeFrame.Day, start = start_time_long_distribution.isoformat(), end = None, limit = LONG_DISTRIBUTION)
@@ -98,16 +104,5 @@ def get_Market_health(api):
     IWM_DIST_LONG = get_Distribution_DAY_COUNT(LONG_DISTRIBUTION_NUMBER, IWM_D_BARSET)
     IWM_DIST_SHORT = get_Distribution_DAY_COUNT(SHORT_DISTRIBUTION_NUMBER, IWM_D_BARSET)
 
-    #Print Distribution Days
-    print("Distribution Day Count($SPY) over last",LONG_DISTRIBUTION_NUMBER, " days: ", SPY_DIST_LONG)
-    print("Distribution Day Count($SPY) over last",SHORT_DISTRIBUTION_NUMBER, " days: ", SPY_DIST_SHORT)
-    print("Distribution Day Count($QQQ) over last",LONG_DISTRIBUTION_NUMBER, " days: ", QQQ_DIST_LONG)
-    print("Distribution Day Count($QQQ) over last",SHORT_DISTRIBUTION_NUMBER, " days: ", QQQ_DIST_SHORT)
-    print("Distribution Day Count($DIA) over last",LONG_DISTRIBUTION_NUMBER, " days: ", DIA_DIST_LONG)
-    print("Distribution Day Count($DIA) over last",SHORT_DISTRIBUTION_NUMBER, " days: ", DIA_DIST_SHORT)
-    print("Distribution Day Count($IWO) over last",LONG_DISTRIBUTION_NUMBER, " days: ", IWO_DIST_LONG)
-    print("Distribution Day Count($IWO) over last",SHORT_DISTRIBUTION_NUMBER, " days: ", IWO_DIST_SHORT)
-    print("Distribution Day Count($IWM) over last",LONG_DISTRIBUTION_NUMBER, " days: ", IWM_DIST_LONG)
-    print("Distribution Day Count($IWM) over last",SHORT_DISTRIBUTION_NUMBER, " days: ", IWM_DIST_SHORT)
-
-    #def print_distribution_day
+    distribution_array = [SPY_DIST_LONG,SPY_DIST_SHORT,QQQ_DIST_LONG,QQQ_DIST_SHORT,DIA_DIST_LONG,DIA_DIST_SHORT,IWO_DIST_LONG,IWO_DIST_SHORT,IWM_DIST_LONG,IWM_DIST_SHORT]
+    print_distribution_day(distribution_array)
