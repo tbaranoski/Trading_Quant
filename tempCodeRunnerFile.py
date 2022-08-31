10864494aff5a3f2f05c79acf2f137258ef6ce9f
tbaranoski <taylorbaranoski1@gmail.com>
2022-08-31 16:30:03 -0400

    Issue #19 Websocket Connection Setup

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-
-            "symbol": symbol,
+BASE_URL = "https://paper-api.alpaca.markets"
--- web_socket.py
+++ web_socket.py
@@ -10,12 +10,8 @@ from alpaca_trade_api.common import URL #not sure if needed
 
 log = logging.getLogger(__name__)
 
-
 ###########################################################################
-###########################################################################
-###########################################################################
-
-
+#Helper Functions taken from V2_example.py
 async def print_trade(t):
     print('trade', t)
 
@@ -31,27 +27,39 @@ async def print_trade_update(tu):
 async def print_crypto_trade(t):
     print('crypto trade', t)
 
+#I added print_daily_bar()
+async def print_daily_bar(db):
+    print('Daily Bar: ', db)    
+
+
+#END of Helper Functions
+###########################################################################
 
 def main():
+    
+    #Set up Websocket
     logging.basicConfig(level=logging.INFO)
     feed = 'iex'  # <- replace to SIP if you have PRO subscription
     stream = Stream(data_feed=feed, raw_data=True, key_id= config.API_KEY, secret_key=config.SECRET_KEY, base_url="https://paper-api.alpaca.markets")
-    stream.subscribe_trade_updates(print_trade_update)
-    stream.subscribe_trades(print_trade, 'AAPL')
-    stream.subscribe_quotes(print_quote, 'IBM')
-    #stream.subscribe_crypto_trades(print_crypto_trade, 'BTCUSD')
+
+    #Start Subscriptions
+    #stream.subscribe_trade_updates(print_trade_update)
+    #stream.subscribe_trades(print_trade, 'AAPL')
+    #stream.subscribe_quotes(print_quote, 'IBM')
+    stream.subscribe_daily_bars(print_daily_bar, 'SPY')
+
 
     @stream.on_bar('MSFT')
     async def _(bar):
         print('bar', bar)
 
-    @stream.on_updated_bar('MSFT')
+    @stream.on_updated_bar('SPY')
     async def _(bar):
         print('updated bar', bar)
 
-    @stream.on_status("*")
-    async def _(status):
-        print('status', status)
+    #@stream.on_status("*")
+    #async def _(status):
+        #print('status', status)
 
     @stream.on_luld('AAPL', 'MSFT')
     async def _(luld):
