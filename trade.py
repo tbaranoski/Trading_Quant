6091bcb152851d9b4c65226cb66aeb6116f110f6
tbaranoski <taylorbaranoski1@gmail.com>
2022-09-19 23:11:49 -0400

    Issue #38 and Issue #34

--- trade.py
+++ trade.py
@@ -3,6 +3,7 @@
 from ast import And
 from email.policy import default
 from pickle import NONE
+from statistics import mode
 from sys import api_version
 from unicodedata import name
 import requests , json
@@ -99,7 +100,7 @@ class Group:
 #Create stock class (Child Class) to store the key data that will be used in the trading strategy.
 #This will allow us to choose different strategies to use for different stocks
 class Stock(Group):
-    def __init__(self, name, current_price_estimate = None, EMA_21 = None, EMA_9 = None, distribution_Short_len = None, distribution_Long_len = None, current_trend = None, strategy = None, data_temp = []):
+    def __init__(self, name, current_price_estimate = None, EMA_21 = None, EMA_9 = None, distribution_Short_len = None, distribution_Long_len = None, current_trend = None, strategy = None, data_temp = [], current_timeframe_string = "Day"):
         self.name = name
         self.current_price_estimate = current_price_estimate        
         self.EMA_21 = EMA_21
@@ -108,6 +109,7 @@ class Stock(Group):
         self.distribution_Long_len = distribution_Long_len
         self.strategy = strategy
         self.dataset = data_temp
+        self.current_timeframe_string = current_timeframe_string 
         
         #If a trend starting value is provided, create object with it
         if(current_trend != None):
@@ -161,17 +163,169 @@ class Stock(Group):
     ##################################################################################################################################
     #Fucntion Analyzes data looking for Higher Highs, Higher Lows, Lower Lows and Lower Highs with Price Action
     def determine_ititial_trend(self):
-        print("QUICK TEST!!!!")
-       
+        ############################################################################################################################
+        ############################################################################################################################
+        ####   CONSTRUCTION START   ####
+        NUM_CANDLES = len(self.dataset)
+        SKIP_INCREMENT = 2 #Every other candle
+        MODE = None #True = Looking for next relative High. False = Looking for next relative Low
+        #last_high = None
+        #last_low = None
+
+        #new_high = None
+        #new_low = None
+
+        #Start trend itteration at the first candle
+        i = 0 + SKIP_INCREMENT
+        while(i < NUM_CANDLES):
+            
+            #Edge Case: Evaluating first candle, determine initial direction and set MODE
+            if(i == SKIP_INCREMENT):
+
+                #direction up initially
+                if(self.dataset[i] > self.dataset[i - SKIP_INCREMENT]):
+                    MODE = True
+                    self.trend.last_high = self.dataset[0]
+
+                #direction down initially
+                else:
+                    MODE = False
+                    self.trend.last_low = self.dataset[0]
+            ###end endge case if
+
+            #Determine if we are looking for Relative High or Relative Low
+            #Looking for High, if current candle is higher mark as new high
+            array = self.find_pivot(i, MODE, SKIP_INCREMENT)
+            i = array[0]
+            self.process_pivot(array[1], MODE)
+            
+            #Negate the MODE
+            MODE = not MODE
+
+            #Update the Trend State Machine
+            self.trend.update_state()
 
 
-    ##################################################################################################################################
+            i = i + SKIP_INCREMENT
+        #### END OF WHILE
+
+    ############################################################################################################################
+    #Helper Function to store the pivot found in appropriate place as well as update counters
+    def process_pivot(self, pivot, mode):
+        
+        #Process New High
+        if(mode == True):
+
+            #Compare to last High
+            #If Higher add to higher high, reset lower high
+            if(pivot > self.trend.last_high):
+                self.trend.higher_high_count = self.trend.higher_high_count + 1
+                self.trend.lower_high_count = 0
+            
+            #If Lower reset higher high, add to lower high
+            else:
+                self.trend.higher_high_count = 0
+                self.trend.lower_high_count = self.trend.lower_high_count + 1
+
+            #Save pivot
+            self.trend.last_high = pivot
+        
+        #############################################################################
+        #Process Low
+        elif(mode == False):
+
+            #Compare to last Low
+            #If lower add to lower low count and reset higher low count
+            if(pivot < self.trend.last_low):
+                self.trend.lower_low_count = self.trend.lower_low_count + 1
+                self.trend.higher_low_count = 0
+            
+            #If higher low reset lower low count and increment higher low
+            else:
+                self.trend.lower_low_count = 0
+                self.trend.higher_low_count = self.trend.higher_low_count + 1
+
+            #Save pivot
+            self.trend.last_low = pivot
+            
+    ####end of function
+    ############################################################################################################################
+
+    #Function Determines if relative high or low was spotted
+    #Leave Function when Found
+    def find_pivot(self, i, mode, SKIP_INCREMENT):
+        up_counter = 0
+        down_counter = 0
+        return_index = None #Returns the index where we left off
+
+        NUM_CANDLES = len(self.dataset)
+        HIGH_CONFIRM = 3
+        LOW_CONFIRM = 3
+
+        #Initial setup of "markers"
+        #Determine to set temporary new_high or new_low value based on the mode
+        if(mode == True):
+            self.trend.new_high = self.dataset[i]
+        elif(mode == False):
+            self.trend.new_low = self.dataset[i]
 
 
+        ##################################################################################
+        ##################################################################################
+        #Increment by one candle at a time
+        while(i < NUM_CANDLES):
+
+            #Looking for HIGH
+            if(mode == True):
+
+                #If new temp_high was made
+                if(self.dataset[i] > self.trend.new_high):
+                    self.trend.new_high = self.dataset[i]
+                    up_counter = 0 #reset counter since new highs being made
+
+                #HIGH might be in place
+                else:
+                    up_counter = up_counter + 1
+
+                #Check counter to see if HIGH can be officially Confirmed
+                #confirmed
+                if(up_counter == HIGH_CONFIRM):
+                    break
+            
+            ################################################################
+            #Looking for LOW
+            elif(mode == False):
+                  
+                #If new temp_low was made
+                if(self.dataset[i] < self.trend.new_low):
+                    self.trend.new_low = self.dataset[i]
+                    down_counter = 0 #reset counter since new highs being made
+
+                #Low might be in place
+                else:
+                    down_counter = down_counter + 1
+
+                #Check counter to see if Low can be officially Confirmed
+                #confirmed
+                if(down_counter == LOW_CONFIRM):
+                    break
+            
+            i = i + 1 #increment the counter
+        #end while
+
+        #Send pivot index and Price back
+        if(mode == True):
+            pivot = self.trend.new_high
+            return_index = i - HIGH_CONFIRM
+
+        elif(mode == False):
+            pivot = self.trend.new_low
+            return_index = i - LOW_CONFIRM
+        
+        return [return_index, pivot]
 
 ############################################################################################################
 ############################################################################################################
-
 #1: confirmed_uptrend: Confirmed Uptrend
 #2: possible_uptrend_reversal: Trend Might Reverse from Uptrend to Downtrend
 #3: confirmed_downtrend: Confirmed Downtrend
@@ -198,9 +352,10 @@ class trend_state(Enum):
 
 class Trend(Stock):
 
-    def __init__(self, current_trend = trend_state.starting_state, trend_hour = trend_state.starting_state, trend_30min = trend_state.starting_state, trend_15min = trend_state.starting_state, trend_5min = trend_state.starting_state, trend_1min = trend_state.starting_state, higher_high_counter = 0, lower_low_counter = 0, higher_low_counter = 0, lower_high_counter = 0):
+    def __init__(self, trend_child_Week = trend_state.starting_state, current_trend = trend_state.starting_state, trend_hour = trend_state.starting_state, trend_30min = trend_state.starting_state, trend_15min = trend_state.starting_state, trend_5min = trend_state.starting_state, trend_1min = trend_state.starting_state, higher_high_counter = 0, lower_low_counter = 0, higher_low_counter = 0, lower_high_counter = 0):
         
         #Trend Attributes for Different TimeFrames
+        self.trend_child_Week = trend_child_Week
         self.trend_child_Day = current_trend #default state if no current_trend is passed in. (Will be used 99% of time). Note: Daily timeframe
         self.trend_child_Hour = trend_hour
         self.trend_child_30min = trend_30min
@@ -214,6 +369,12 @@ class Trend(Stock):
         self.lower_low_counter = lower_low_counter
         self.lower_high_counter = lower_high_counter
 
+        #temporary trackers
+        self.last_high = None
+        self.last_low = None
+        self.new_high = None
+        self.new_low = None
+
     ##################################################################
 
     #Resets trend markers to default when computing trend for a new timeframe
@@ -226,6 +387,162 @@ class Trend(Stock):
     #Print Main Daily Trend
     def print_trend_D(self):
         return (self.trend_child_Day.name)
+
+    ####################################################################################################
+    ####################################################################################################
+    #STATE MACHINE FUNCTION
+    def update_state(self):
+
+        last_state_temp = None
+
+        #Get Last state first. Pull appropriate varibale coresponding to that timeframe.
+        #Essentially case statement but case is only supported python 3.10
+        if(self.current_timeframe_string == "Week"):
+            last_state_temp = self.trend_child_Week
+        if(self.current_timeframe_string == "Day"):
+                last_state_temp = self.trend_child_Day
+        if(self.current_timeframe_string == "Hour"):
+            last_state_temp = self.trend_child_Hour
+        if(self.current_timeframe_string == "30min"):
+            last_state_temp = self.trend_child_30min
+        if(self.current_timeframe_string == "15min"):
+            last_state_temp = self.trend_child_15min
+        if(self.current_timeframe_string == "5min"):
+            last_state_temp = self.trend_child_5min
+        if(self.current_timeframe_string == "1min"):
+            last_state_temp = self.trend_child_1min
+
+        state_machine(last_state_temp)
+
+####################################################################################################
+####################################################################################################
+    def state_machine(self, last_state):        
+
+        #temp variable
+        new_state = None
+
+        #Based on the last State update the current State for that trend        
+        
+        #State 1: confirmed_uptrend
+        if(last_state == trend_state.confirmed_uptrend):
+
+            #Possibilities include: go back to state 1 or go to state 2 (6 combinations)
+            #State :1 -> 2 (combination 1)
+            if((self.higher_high_counter == 0) and ( self.higher_low_counter >= 2) and (self.lower_low_counter == 0) and (self.lower_high_counter == 1)):
+                new_state = trend_state.possible_uptrend_reversal
+
+            #State :1 -> 2 (combination 2)
+            elif((self.higher_high_counter == 0) and ( self.higher_low_counter == 0) and (self.lower_low_counter == 1) and (self.lower_high_counter == 1)):
+                new_state = trend_state.possible_uptrend_reversal
+
+            #State :1 -> 2 (combination 3)
+            elif((self.higher_high_counter == 0) and ( self.higher_low_counter == 0) and (self.lower_low_counter == 1) and (self.lower_high_counter == 2)):
+                new_state = trend_state.possible_uptrend_reversal
+
+            #State :1 -> 2 (combination 4)
+            elif((self.higher_high_counter >= 2) and ( self.higher_low_counter == 0) and (self.lower_low_counter == 1) and (self.lower_high_counter == 0)):
+                new_state = trend_state.possible_uptrend_reversal
+
+            #State :1 -> 2 (combination 5)
+            elif((self.higher_high_counter == 0) and ( self.higher_low_counter == 0) and (self.lower_low_counter == 1) and (self.lower_high_counter == 1)):
+                new_state = trend_state.possible_uptrend_reversal
+
+            #State :1 -> 2 (combination 6)
+            elif((self.higher_high_counter == 0) and (self.higher_low_counter == 0) and (self.lower_low_counter == 2) and (self.lower_high_counter == 1)):
+                new_state = trend_state.possible_uptrend_reversal
+
+            #Stay at confirmed Uptrend State: 1 - > 1
+            else:
+                new_state = last_state
+
+
+        #State 2: possible_uptrend_reversal to downside
+        if(last_state == trend_state.possible_uptrend_reversal):
+
+            #Four possible Scenarios: Stay at state 2, go to state 6, go to state 5 or go to state 3
+            #State: 2 -> 6
+            if((self.higher_high_counter >= 2) and ( self.higher_low_counter == 0) and (self.lower_low_counter >= 2) and (self.lower_high_counter == 0)):
+                new_state = trend_state.volitility_expansion
+
+            #State: 2 -> 5
+            elif((self.higher_high_counter == 0) and ( self.higher_low_counter >= 2) and (self.lower_low_counter == 0) and (self.lower_high_counter >= 2)):
+                new_state = trend_state.volitility_contraction
+
+            #State: 2 -> 3
+            elif((self.higher_high_counter == 0) and ( self.higher_low_counter == 0) and (self.lower_low_counter >= 2) and (self.lower_high_counter >= 2)):
+                new_state = trend_state.confirmed_downtrend
+
+            #Stay at State. State: 2 -> 2
+            else((self.higher_high_counter == 0) and ( self.higher_low_counter == 0) and (self.lower_low_counter >= 2) and (self.lower_high_counter >= 2)):
+                new_state = trend_state.possible_downtrend_reversal
+
+
+
+        #State 3: confirmed_downtrend
+        if(last_state == trend_state.confirmed_downtrend):
+
+            #Possibilities include: go back to state 3 or go to state 4 (6 combinations)
+            #State:3 -> 4 (combination 1)
+            if((self.higher_high_counter == 1) and ( self.higher_low_counter == 0) and (self.lower_low_counter >= 2) and (self.lower_high_counter == 0)):
+                new_state = trend_state.possible_downtrend_reversal
+
+            #State:3 -> 4 (combination 2)
+            if((self.higher_high_counter == 1) and ( self.higher_low_counter == 1) and (self.lower_low_counter == 0) and (self.lower_high_counter == 0)):
+                new_state = trend_state.possible_downtrend_reversal
+
+            #State:3 -> 4 (combination 3)
+            if((self.higher_high_counter == 2) and ( self.higher_low_counter == 1) and (self.lower_low_counter == 0) and (self.lower_high_counter == 0)):
+                new_state = trend_state.possible_downtrend_reversal
+
+            #State:3 -> 4 (combination 4)
+            if((self.higher_high_counter == 0) and ( self.higher_low_counter == 1) and (self.lower_low_counter == 0) and (self.lower_high_counter >= 2)):
+                new_state = trend_state.possible_downtrend_reversal
+
+            #State:3 -> 4 (combination 5)
+            if((self.higher_high_counter == 1) and ( self.higher_low_counter == 1) and (self.lower_low_counter == 0) and (self.lower_high_counter == 0)):
+                new_state = trend_state.possible_downtrend_reversal
+
+            #State:3 -> 4 (combination 6)
+            if((self.higher_high_counter == 1) and (self.higher_low_counter == 2) and (self.lower_low_counter == 0) and (self.lower_high_counter == 0)):
+                new_state = trend_state.possible_downtrend_reversal
+
+            #Stay at confirmed downtrend State 3 -> 3
+            else:
+                new_state = last_state
+
+
+        #State 4: possible_downtrend_reversal
+        if(last_state == trend_state.possible_downtrend_reversal):
+
+
+        #State 5: volitility_contraction
+        if(last_state == trend_state.volitility_contraction):
+
+
+        #State 6: volitility_expansion
+        if(last_state == trend_state.volitility_expansion):
+
+
+        #State 7: starting_state
+        if(last_state == trend_state.starting_state):
+            
+            #Can only go to state 1 or 3 or stay same
+            #State :7 -> 1
+            if((self.higher_high_counter >= 2) and ( self.higher_low_counter >= 2) and (self.lower_low_counter == 0) and (self.lower_high_counter == 0)):
+                new_state = trend_state.confirmed_uptrend
+
+            #State :7 -> 3
+            elif((self.higher_high_counter == 0) and ( self.higher_low_counter == 0) and (self.lower_low_counter >= 2) and (self.lower_high_counter >= 2)):
+                new_state = trend_state.confirmed_downtrend
+            
+            #Stay at State
+            else:
+                new_state = last_state
+
+        #State 8: debug# Used for troubleshooting
+        if((last_state == trend_state.debug) or (new_state == None)):
+            logging.error("Error: Reached State 8 (New State likelly not set correctly in state machine)")
+
 
 ############################################################################################################
 ############################################################################################################
